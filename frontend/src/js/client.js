6a53a1d535aa6e43e1ae3c34ef94497f7495ae38
nate <nate@blend.com>
2018-09-27 18:11:24 -0400

    save

---
+++ frontend/src/js/client.js
@@ -0,0 +1,12 @@
+// class App extends React.Component {
+//   render() {
+//     return <p>React App</p>;
+//   }
+// }
+// 
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root')
+// );
+
+console.log('hi');
