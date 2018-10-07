983e0f05dfa3fafea92c64a4e76ade5c4c9da383
nate <nate@blend.com>
2018-10-07 17:50:48 +0800

    tags work not perf

--- frontend/src/js/components/questions/Tags.js
+++ frontend/src/js/components/questions/Tags.js
@@ -9,37 +9,23 @@ export class Tags extends React.Component {
     };
   }
   render() {
-    const temp = (
-      <div className="field is-grouped">
-        <div className="control">
-          <input className="input is-expanded" placeholder="Add Tag" value={this.state.currentValue} onChange={this.handleInputChange.bind(this)}/>
-        </div>
-        <div className="control">
-          <div className="tags">
-            {_.map(this.props.tags, (value, listId) =>
-              <span key={uuid.v4()} className="tag is-info is-medium">{value}<button className="delete is-small" onClick={this.props.handleQuestionTagsDelete.bind(this, listId)}/></span>
-            )}
-          </div>
-        </div>
-        <div className="control">
-          <button className="button" type="submit">Save Tag</button>
-        </div>
-      </div>
-    );
-    const debug = (
-      <div class="field is-grouped is-grouped-multiline">
-        {_.map(this.props.tags, (value, listId) =>
-          <div className="control">
-            <div className="tags has-addons">
-              <span key={uuid.v4()} className="tag is-info">{value}<button className="delete" onClick={this.props.handleQuestionTagsDelete.bind(this)}></button></span>
-            </div>
-          </div>
-        )}
-      </div>
-    );
     return (
       <form onSubmit={this.handleTagSave.bind(this)} hidden={this.props.type !== QUESTION_TYPES.VALUES}>
-        {debug}
+        <div class="field is-grouped is-grouped-multiline">
+          <div className="control">
+            <input className="input is-expanded" placeholder="Add Tag" value={this.state.currentValue} onChange={this.handleInputChange.bind(this)}/>
+          </div>
+          {_.map(this.props.tags, (value, listId) =>
+            <div className="control">
+              <div className="tags has-addons">
+                <span key={uuid.v4()} className="tag is-info">{value}<button className="delete" onClick={this.props.handleQuestionTagsDelete.bind(this)}></button></span>
+              </div>
+            </div>
+          )}
+          <div className="control">
+            <button className="button" type="submit">Save Tag</button>
+          </div>
+        </div>
       </form>
     );
   }
