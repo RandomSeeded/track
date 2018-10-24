1b5f60b6cf0e6e807e16a1932d66bc93f82dad77
nate <nate@blend.com>
2018-10-24 18:38:40 -0400

    title and deploy

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ aws ecr get-login --no-include-email --profile RandomSeed | /bin/bash
 docker build -t randomseed .
 docker tag randomseed:latest 463792394146.dkr.ecr.us-east-2.amazonaws.com/randomseed:latest
 docker push 463792394146.dkr.ecr.us-east-2.amazonaws.com/randomseed:latest
+aws ecs update-service --cluster default --service longitude-service --force-new-deployment --profile RandomSeed
+
