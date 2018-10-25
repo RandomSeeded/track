8ecb3b68e0fddf5b3f36896f7a2ad7419ecfe694
nate <nate@blend.com>
2018-10-24 22:53:24 -0400

    save

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+cd frontend && npm run build && cd ..
 aws ecr get-login --no-include-email --profile RandomSeed | /bin/bash
 docker build -t randomseed .
 docker tag randomseed:latest 463792394146.dkr.ecr.us-east-2.amazonaws.com/randomseed:latest
