aac57c788381870d563b37713e5c2c0a787b01e9
nate <nate.a.willard@gmail.com>
2018-10-28 20:32:02 -0700

    frontend hookup for reminder set

--- frontend/src/js/components/reminders/index.js
+++ frontend/src/js/components/reminders/index.js
@@ -1,17 +1,25 @@
 import * as _ from 'lodash';
+import * as axios from 'axios';
 
 export class Reminders extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneNumber: '',
+    };
+  }
+
   render() {
     return (
       <div>
         <div className="section">
-          <form>
+          <form onSubmit={this.handleSubmit.bind(this)}>
             <div className="field">
               <label className="label">What's your phone #?</label>
-              <input className="input"/>
+              <input className="input" onChange={this.handleChange.bind(this)} value={this.state.phoneNumber} placeholder="+1 234 567 8901"/>
             </div>
             <div className="field">
-              <button className="button is-primary">Save</button>
+              <button className="button is-primary" type="submit">Save</button>
             </div>
           </form>
         </div>
@@ -20,5 +28,20 @@ export class Reminders extends React.Component {
         </div>
       </div>
     );
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    const phoneNumber = event.target.value;
+    this.setState({
+      phoneNumber,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const phoneNumber = this.state.phoneNumber;
+    const body = { phoneNumber };
+    await axios.post('/api/users/phone-number', body);
   }
 }
