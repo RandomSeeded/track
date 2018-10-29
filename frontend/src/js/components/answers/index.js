c669962dbff0fdfbc917acf00f83d45c12f07ef3
nate <nate.a.willard@gmail.com>
2018-10-28 20:04:02 -0700

    mobile-friendly answer nav

--- frontend/src/js/components/answers/index.js
+++ frontend/src/js/components/answers/index.js
@@ -5,16 +5,16 @@ class AnswerChooser extends React.Component {
   render() {
     const currentDate = format(Number(this.props.currentDate), 'MM/DD/YY hh:mm A');
     return (
-      <div className="level">
+      <div className="level is-mobile">
         <div className="buttons has-addons level-item">
-          <a className="button is-large" disabled={this.props.isFirstDate} onClick={this.props.handlePrevDate}>
-            <span className="icon is-large">
+          <a className="button" disabled={this.props.isFirstDate} onClick={this.props.handlePrevDate}>
+            <span className="icon">
               <i className="fas fa-chevron-left"/>
             </span>
           </a>
-          <span className="button is-large is-primary">{currentDate}</span>
-          <a className="button is-large" disabled={this.props.isLastDate} onClick={this.props.handleNextDate}>
-            <span className="icon is-large">
+          <span className="button is-primary">{currentDate}</span>
+          <a className="button" disabled={this.props.isLastDate} onClick={this.props.handleNextDate}>
+            <span className="icon">
               <i className="fas fa-chevron-right"/>
             </span>
           </a>
